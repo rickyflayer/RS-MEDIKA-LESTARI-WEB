74ea0d0aec7f7f908ed2202a79af9ccea0c43ab0
yosuamau <50090632+yosuamau@users.noreply.github.com>
2020-02-05 16:36:35 +0700

    Edit About Linked Profile + Asuransi
    Edit Profil + Asuransi

---
+++ php_rs/about.php
@@ -0,0 +1,57 @@
+<?php include('top.php');?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Profile</title>
+	</head>
+	<body>
+	<div class="about-page">
+	<div class="container">
+		<h3 class="tittle wow fadeInDown animated" data-wow-delay=".5s">Profile</h3>
+            <div class="col-md-6 about_img wow fadeInLeft animated" data-wow-delay=".5s">
+				<img src="images/g1.jpg" alt="">
+            </div>
+			<div class="col-md-6 about_img about_img_right">
+				<div class="choose-grid wow fadeInRight animated" data-wow-delay=".5s">
+					<h4>Sejarah Rs. Medika Lestari</h4>
+					<p>	Rumah Sakit Medika Lestari berdiri tanggal 15 oktober 1994, berawal dari sebuah klinik yang bertempat di Jl. HOS Cokroaminoto Perum Pondok Lestari Blok C1 No.1-2, Jl. HOS Cokroaminoto, Ciledug Kota Tanggerang 15157. Medika Lestari hadir untuk memenuhi permintaan masyarakat akan kesehatan yang baik dan prima. Pada tahun 1997 Klinik Medika Lestari membangun gedung baru dan meningkatkan statusnya dari klinik menjadi Klinik Spesialis dan Rumah Bersalin Medika Lestari. Pada tahun 2005 Rumah Sakit Medika Lestari menambah fasilitas dan meningkatkan statusnya menjadi Rumah Sakit Ibu dan Anak (RSIA), dan selanjutnya pada tahun 2008 Rumah Sakit Medika Lestari meningkatkan statusnya menjadi Rumah Sakit Umum, dan tahun 2012 Rumah Sakit Medika Lestari terus berkembang, dengan Motivasi yang sangat tinggi serta kebersamaan dan kekompakan seluruh karyawan RS Medika Lestari, kini RS Medika Lestari sampai pada proses pembangunan serta pengembangan dari segi pelayanan serta fasilitas. Tahun 2017 sampai sekarang RS Medika Lestari sudah melayani Rawat Inap, Rawat Jalan, dan layanan 24 Jam (IGD, Radiologi, Laboratorium, Farmasi) dan sudah terakreditasi.</p>
+				</div>
+				<div class="choose-grid wow fadeInLeft animated" data-wow-delay=".5s">
+					<h4>Tujuan Rs. Medika Lestari</h4>
+					<p>- Maintance -</p>
+				</div>
+            </div>
+			<div class="clearfix"></div>
+		<p class="para_abt wow fadeInUp animated" data-wow-delay=".5s">Kesembuhan Anda Kebahagiaan Kami</p>
+	</div>
+</div>
+<!-- visi & misi-->
+	<div class="testimonials">
+		<div class="container">
+			<h3 class="tittle">Visi & Misi</h3>
+			<div class="testimonials-grids">
+				<div class="col-md-6 testimonials-grid animated wow slideInLeft" data-wow-delay=".5s">
+					<div class="testimonials-grid1">
+						<img src="images/visi.png" alt=" " class="img-responsive" />
+						<p>Visi</p>
+					</div>
+					<div class="testimonials-grid2">
+						<p>Menjadikan Rumah Sakit yang terbaik dan terjangkau oleh masyarakat</p>
+					</div>
+				</div>
+				<div class="col-md-6 testimonials-grid animated wow slideInRight" data-wow-delay=".5s"">
+					<div class="testimonials-grid1">
+						<img src="images/misi.png" alt=" " class="img-responsive" />
+						<p>Misi</p>
+					</div>
+					<div class="testimonials-grid2">
+						<p>1. Mengutamakan Pasien Safety, <br>2. Meningkatkan mutu pelayanan secara berkesinambungan,<br>3. Memberikan pelayanan yang efektif dan efisien.</p>
+					</div>
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+		</div>
+	</div>                      
+</body>
+<?PHP include('bottom.php');?> 
+</html>    
---
+++ php_rs/asuransi.php
@@ -0,0 +1,42 @@
+<?php include('top.php')?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Asuransi</title>
+</head>
+<body>
+	<div class="about-page">
+	<div class="container">
+		<h3 class="tittle wow fadeInDown animated" data-wow-delay=".5s">Asuransi Rs. Medika Lestari</h3>
+            <div class="col-md-6 about_img about_img_Left">
+				<div class="choose-grid wow fadeInLeft animated" data-wow-delay=".5s">
+					<h4>Asuransi BPJS</h4>
+					<p>	Kami bekerjasama dengan Badan Usaha Milik Negara yang ditugaskan khusus oleh pemerintah untuk menyelenggarakan jaminan pemeliharaan kesehatan bagi seluruh rakyat Indonesia. pasien dapat menggunakan asuransi bpjs dalam pembiayaan untuk perawatan kesehatan yang dilakukan oleh rumah sakit. BPJS kesehatan dapat digunakan dalam semua layanan kesehatan yang ada pada rumah sakit.<br>
+						Kami memberikan kemudahan penerimaan pasien dan dalam pembayaran biaya kesehatan dengan asuransi BPJS, tim rumah sakit kami telah bekerjasama dengan BPJS kesehatan dan semua pasien yang memilikinya dapat melakukan pemeriksaan kesehatan, pengobatan, dan penyembuhan dengan sangat mudah dan cepat.</p>
+				</div>
+		</div>	
+        <div class="col-md-6 about_img wow fadeInRight animated" data-wow-delay=".5s">
+				<img src="images/g1.jpg" alt="" class="about_img_right">
+            </div>	
+	</div>
+	<div class="container">
+	<div class="col-md-8 about_img about_img_Left">
+	<div class="choose-grid wow fadeInLeft animated" data-wow-delay=".5s">
+					<h4>Daftar Asuransi & Perusahaan Yang Bekerja Sama Dengan <br>Rs. Medika Lestari</h4>
+					<div class="col-md-6 about_img about_img_Left">
+					<div class="choose-grid wow slideInLeft animated " data-wow-delay=".5s">
+					<p>1. PT. Asuransi Lippo Insurance (MEDICARE)<br> 2. PT. Asuransi Sinar Mas (SIMAS)<br> 3. PT. Asuransi Multi Artha Guna (MAGNA)<br> 4. PT. Asuransi Astra Buana (GARDA MEDIKA)<br> 5. PT. Asuransi Gesa Assistance (SIMAS)<br> 6. PT. Asuransi Central Asia Raya (CAR)<br> 7. PT. Asuransi Bina Dana Arta (ABDA)<br> 8. PT. Asuransi Umum Mega (MEGA INSURANCE)<br> 9. PT. Asuransi Tugu Mandiri (TM)<br> 10. PT. Asuransi Jasa Indonesia (JASINDO)<br> 11. PT. Asuransi Adira Dinamika (Medicillin)<br> 12. PT. Asuransi I&#39;M Care 177<br>13. Yayasan Tarakanita</p>
+					</div>
+					</div>
+					<div class="col-md-6 about_img about_img_Right">
+					<div class="choose-grid wow slideInRight animated" data-wow-delay=".5s">
+					<p>14. PT. Champ Resto<br>15. PT. Abadi Smilynks<br>16. PT. AA International Indonesia<br>17. PT. Syntech Mitra Integrasi<br>18. PT. Asuransi Platinum Platinum Health Care<br>19. PT. Administrasi Medika<br>20. PT. Asuransi Jiwa Sequis Life<br>21. PT. Hanwa Life Insurance Indonesia<br>22. PT. Asuransi Ramayana<br>23. PT. NTT Data<br>24. PT. Asuransi FWD<br>25. PT. Aplikanusa Lintasarta (OWLEXA)<br>26. PT. Asuransi Bosowa<br>27. PT. Asuransi Brigin Life<br></p>
+					</div>
+					</div>
+		</div>
+	</div>
+	</div>
+	</div>				
+</body>
+</html>
+<?php include('bottom.php')?>
